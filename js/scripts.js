e1f71c4bbc661d54d2dcd966c8e1f9d974c09166
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2019-08-17 18:03:14 +0300

    Каркас сайта завершен

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// $(document).ready(function () {
+//     $('.our-clients').slick({
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//     });
+// });
